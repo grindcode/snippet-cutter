2a1291c1f8a3af94a9483cd361049a85336949a3
Isis T. Baulig <grindcode@gmail.com>
2016-01-08 16:07:52 +0100

    avoid arrow function in favor of ancient JavaScript support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var defaults = require('lodash.defaults')
 module.exports = function (str, opts) {
   var options = defaults({}, opts, { min: 1, max: 140, ellipsis: '...'})
-  return str.split('.').reduce((candidate, sentence) => {
+  return str.split('.').reduce(function (candidate, sentence) {
     return (candidate.length < options.min)
            ? candidate.concat(`${sentence}.`)
            : (candidate.length > (options.max + options.ellipsis.length))
