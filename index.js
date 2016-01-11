e1814da7366a82096ac55d279c5756108f4c1b5b
Isis T. Baulig <grindcode@gmail.com>
2016-01-11 13:15:04 +0100

    add delimeter option

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var defaults = require('lodash.defaults')
 module.exports = function (str, opts) {
-  var options = defaults({}, opts, { min: 1, max: 140, ellipsis: '...'})
-  return str.split('.').reduce(function (candidate, sentence) {
+  var options = defaults({}, opts, {
+    min: 1, max: 140, ellipsis: '...', delimeter: '.'
+  })
+  return str.split(options.delimeter).reduce(function (candidate, sentence) {
     return (candidate.length < options.min)
            ? candidate.concat(`${sentence}.`)
            : (candidate.length > (options.max + options.ellipsis.length))
              ? candidate.slice(0, options.max) + options.ellipsis
-             : candidate.trim()
+             : candidate
   }, '')
 }
