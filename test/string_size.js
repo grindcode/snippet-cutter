e67457e6886314cc4cd4898fc5c8a9ff836cfe09
Isis T. Baulig <grindcode@gmail.com>
2016-01-11 13:26:11 +0100

    fix test styles

--- test/string_size.js
+++ test/string_size.js
@@ -7,70 +7,60 @@ var description = "My money's in that office, right? If she start giving me some
 * @result: My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm...
 */
 test('defaults', function (t) {
-    t.plan(1);
-
+    t.plan(1)
     var snippet = cutter(description)
-
-    t.equal(snippet.length, 143);
+    t.equal(snippet.length, 143)
 });
 
 /**
 * @result: My money's in that office, right? If she start giving me some bullshit about it ...
 */
 test('0.5 sentence + ellipsis', function (t) {
-    t.plan(1);
-
+    t.plan(1)
     var snippet = cutter(description, {
       min: 70,
       max: 80,
       ellipsis: '...'
     })
-
-    t.equal(snippet.length, 83);
+    t.equal(snippet.length, 83)
 });
 
 /**
 * @result: My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there.
 */
 test('1 sentence', function (t) {
-    t.plan(1);
-
+    t.plan(1)
     var snippet = cutter(description, {
       min: 170,
       max: 200,
       ellipsis: '...'
     })
-
-    t.equal(snippet.length, 184);
+    t.equal(snippet.length, 184)
 });
 
 /**
 * @result: My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in ...
 */
 test('1.5 sentences + ellipsis', function (t) {
-    t.plan(1);
-
+    t.plan(1)
     var snippet = cutter(description, {
       min: 200,
       max: 220,
       ellipsis: '...'
     })
-
     // 220 + 3
-    t.equal(snippet.length, 223);
+    t.equal(snippet.length, 223)
 });
 
 /**
 * @result: My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is.
 */
 test('2 sentences', function (t) {
-    t.plan(1);
-
+    t.plan(1)
     var snippet = cutter(description, {
       min: 260,
       max: 280,
       ellipsis: '...'
     })
-
-    t.equal(snippet.length, 269);
+    t.equal(snippet.length, 269)
 });
