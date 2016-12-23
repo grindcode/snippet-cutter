092ab38a8946e598d16a082ae6429c875e5eb465
Isis T. Baulig <grindcode@gmail.com>
2016-12-23 14:08:03 +0100

    refactor into more readable code

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var defaults = require('lodash.defaults')
+
+var defaultOptions = {
+  min: 1,
+  max: 140,
+  ellipsis: '...',
+  delimeter: '.',
+}
+
 module.exports = function (str, opts) {
-  var options = defaults({}, opts, {
-    min: 1, max: 140, ellipsis: '...', delimeter: '.'
-  })
-  return str.split(options.delimeter).reduce(function (candidate, sentence) {
-    return (candidate.length < options.min)
-           ? candidate.concat(sentence + options.delimeter)
-           : (candidate.length > (options.max + options.ellipsis.length))
-             ? candidate.slice(0, options.max) + options.ellipsis
-             : candidate
+  var options = defaults({}, opts, defaultOptions)
+  return str.split(options.delimeter).reduce(function (snippet, sentence) {
+    if (snippet.length < options.min) {
+      return snippet.concat(sentence + options.delimeter)
+    }
+    if (snippet.length > (options.max + options.ellipsis.length)) {
+      return snippet.slice(0, options.max) + options.ellipsis
+    }
+    return snippet
   }, '')
 }
